9b5ad883661ff25a784035af0429848cba35e723
nero31994 <arenaofvalorph937@gmail.com>
2025-03-23 04:29:40 +0800

    Update combine-m3u.js

--- api/combine-m3u.js
+++ api/combine-m3u.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
     const urls = [
         "https://premiumm3u.vercel.app/CIGNAL.m3u",
-        "https://premiumm3u.vercel.app/dadylive.m3u","https://premiumm3u.vercel.app/hbogoasia.m3u"
+        "https://premiumm3u.vercel.app/daddylive.m3u","https://premiumm3u.vercel.app/hbogoasia.m3u"
     ];
 
     try {
