ddc9fad4ad9174df8a59f2e3ab3ad4aa2a311174
nero31994 <arenaofvalorph937@gmail.com>
2025-04-04 23:46:05 +0800

    Update combine-m3u.js

--- api/combine-m3u.js
+++ api/combine-m3u.js
@@ -1,8 +1,8 @@
 export default async function handler(req, res) {
     const urls = [
-        "https://premiumm3u.vercel.app/CIGNAL.m3u",
-        "https://premiumm3u.vercel.app/Jungo.m3u",
         "https://premiumm3u.vercel.app/converge.m3u",
+        "https://premiumm3u.vercel.app/CIGNAL.m3u",
+        "https://premiumm3u.vercel.app/Jungo.m3u",   
         "https://premiumm3u.vercel.app/moveonjoy.m3u8",
         "https://iptv-scraper-re.vercel.app/pixelsport",
         "https://raw.githubusercontent.com/nero31994/PLUTO/27f2fb78218684c0f9f0e38ea7f4e6d656fc7832/pluto",
